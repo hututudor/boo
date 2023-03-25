7d8ffe277e3d58af1927af3cf6eaae29ee0d5f3b
Tudor Hutu <hutu.tudor@yahoo.com>
2023-03-25 21:00:39 +0200

    feat(login): add login page and utils

--- client/build.sh
+++ client/build.sh
@@ -6,6 +6,10 @@ cp -r src/pages/* build
 mkdir -p build/scripts
 cp -r src/scripts/* build/scripts
 
+# copy assets
+mkdir -p build/assets
+cp -r src/assets/* build/assets
+
 # build sass files
 mkdir -p build/styles
-sass src/style/pages/auth.scss build/styles/auth.css
+sass src/style/pages/auth.scss build/styles/auth.css --style compressed
