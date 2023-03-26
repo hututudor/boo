14e8a17928a8de716e65dc95b1e164aefa5b2bab
Tudor Hutu <hutu.tudor@yahoo.com>
2023-03-26 13:09:08 +0300

    chore: restructure the project to allow for hot reloading

--- client/build.sh
+++ client/build.sh
@@ -1,15 +1,2 @@
-# copy html
-mkdir -p build
-cp -r src/pages/* build
-
-# copy js
-mkdir -p build/scripts
-cp -r src/scripts/* build/scripts
-
-# copy assets
-mkdir -p build/assets
-cp -r src/assets/* build/assets
-
-# build sass files
-mkdir -p build/styles
-sass src/style/pages/auth.scss build/styles/auth.css --style compressed
+mkdir -p public/styles
+sass src/pages/auth.scss public/styles/auth.css --style compressed
