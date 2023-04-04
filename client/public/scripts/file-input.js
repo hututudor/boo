8d8d5aad6714713fb9fa127b015e47261969e9a8
Tudor Hutu <hutu.tudor@yahoo.com>
2023-04-04 10:27:07 +0300

    client: add/edit books

---
+++ client/public/scripts/file-input.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', handleFormFiles, false);
+
+function handleFormFiles() {
+  const formFiles = document.getElementsByClassName('form-file');
+
+  for (const formFile of formFiles) {
+    const fileInput = formFile.getElementsByTagName('input')[0];
+
+    if (!formFile.getElementsByTagName('input')[0]) {
+      console.error('file input not found');
+      return;
+    }
+
+    if (!formFile.getElementsByTagName('div')[0]) {
+      console.error('file display not found');
+      return;
+    }
+
+    formFile.addEventListener('click', handleFileOpen);
+    fileInput.addEventListener('change', handleFileChange);
+  }
+}
+
+function handleFileChange(event) {
+  const formFile = event.currentTarget.parentElement;
+  const formDisplay = formFile.getElementsByTagName('div')[0];
+  const fileInput = formFile.getElementsByTagName('input')[0];
+
+  formDisplay.innerHTML = fileInput.files[0].name;
+}
+
+function handleFileOpen(event) {
+  const fileInput = event.currentTarget.getElementsByTagName('input')[0];
+
+  fileInput.click();
+}
