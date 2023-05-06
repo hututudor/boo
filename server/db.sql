d6728f5c86dcd7bdc6985748c063ce7999b2a6a5
Tudor Hutu <hutu.tudor@yahoo.com>
2023-05-06 09:00:31 +0300

    fix(db.sql): rename table

--- server/db.sql
+++ server/db.sql
@@ -1,4 +1,5 @@
 drop table if exists books;
+drop table if exists users;
 
 create table books (
   id int not null auto_increment,
@@ -8,7 +9,7 @@ create table books (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
 id INT AUTO_INCREMENT PRIMARY KEY,
 full_name VARCHAR(255) NOT NULL,
 email VARCHAR(255) UNIQUE NOT NULL,
