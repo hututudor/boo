6ac5a4c4f3a38efc339fe0bfd73484fbad675793
Hutu Tudor <hutu.tudor@yahoo.com>
2023-06-17 18:08:38 +0300

    fix(client): fix reviews title

--- server/templates/reviews.php
+++ server/templates/reviews.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>All books | Boo</title>
+  <title>My reviews | Boo</title>
   <link rel="stylesheet" href="<?php path('fork-awesome.css'); ?>" />
   <link rel="stylesheet" href="<?php path('styles/index.css') ?>" />
   <link rel="shortcut icon" href="<?php path('assets/favicon.ico') ?>" type="image/x-icon" />
