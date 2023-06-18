848368b4c4fead39c0cf0f76df962366e6540d2f
Hutu Tudor <hutu.tudor@yahoo.com>
2023-06-18 20:49:25 +0300

    feat(client): show home book status

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,30 @@
-import { getAnalytics } from '../api/analytics';
-import { loadBookImages, renderSidebar } from '../components';
+import { getAnalytics, getHomeBooks } from '../api/analytics';
+import { getBookCardNode, renderSidebar } from '../components';
 
 export const load = async () => {
   renderSidebar();
 
   await displayAnalytics();
+  await displayBooks();
+};
+
+const displayBooks = async () => {
+  const { read, toRead, reading } = await getHomeBooks();
+
+  displayBookRow(read, 'books-read');
+  displayBookRow(reading, 'books-progress');
+  displayBookRow(toRead, 'books-want-to-read');
+};
+
+const displayBookRow = (books, name) => {
+  if (!books?.length) {
+    document.getElementById(`${name}-wrapper`).style.display = 'none';
+    return;
+  }
+
+  const booksNode = document.getElementById(name);
+  booksNode.innerHTML = '';
+  books.forEach(book => booksNode.appendChild(getBookCardNode(book)));
 };
 
 const displayAnalytics = async () => {
