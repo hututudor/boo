4a27912e4daf2c1496d7be8d1fbff3ab2d64a260
cristian-roman <cristian.roman@feel-it-services.com>
2023-06-21 00:29:21 +0300

    database prepared for RSS feed

--- server/db.sql
+++ server/db.sql
@@ -1,9 +1,11 @@
 drop table if exists books;
 drop table if exists users;
 drop table if exists reviews;
+drop table if exists user_books;
 
 create table books (
   id int not null auto_increment,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   image varchar(256),
   title varchar(256) not null,
   author varchar(256) not null,
@@ -19,32 +21,48 @@ create table books (
 
 CREATE TABLE users (
 id INT AUTO_INCREMENT PRIMARY KEY,
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 full_name VARCHAR(255) NOT NULL,
 email VARCHAR(255) UNIQUE NOT NULL,
 password VARCHAR(255) NOT NULL,
 is_admin BOOLEAN DEFAULT FALSE
 );
 
-CREATE OR REPLACE TABLE user_books
+CREATE TABLE user_books
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
-    status  ENUM ('want to read', 'reading', 'read', 'didn''t read') NOT NULL,
+    status ENUM ('want to read', 'reading', 'read', 'didn''t read') NOT NULL,
+    last_seen_review_id INT,
     CONSTRAINT user_books_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT user_books_ibfk_2 FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE,
+    CONSTRAINT user_books_ibfk_3 FOREIGN KEY (last_seen_review_id) REFERENCES reviews (id) ON DELETE CASCADE,
     CONSTRAINT unique_user_book_pair UNIQUE (user_id, book_id)
 );
 
 CREATE INDEX book_id ON user_books (book_id);
 CREATE INDEX user_id ON user_books (user_id);
 
+
 CREATE TABLE reviews (
   id INT AUTO_INCREMENT PRIMARY KEY,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   book_id INT,
   user_id INT,
   content VARCHAR(1000) NOT NULL,
   review_date varchar(256),
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rss_books
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INT,
+    last_seen_book_id INT,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_book FOREIGN KEY (last_seen_book_id) REFERENCES books (id) ON DELETE CASCADE
 );
